d114225512e5516b3c0a16a53be78749e37a17ae
cedmail <cmailleux@jahia.com>
2017-03-14 08:34:22 -0400

    Fix for next iteration of

--- src/main/java/org/jahia/modules/ffmailchimp/initializer/MailChimpInterestsInitializer.java
+++ src/main/java/org/jahia/modules/ffmailchimp/initializer/MailChimpInterestsInitializer.java
@@ -49,7 +49,34 @@ public class MailChimpInterestsInitializer implements ModuleChoiceListInitialize
                 String listId = mailchimpConfiguration.getPropertyAsString("listId");
                 String server = apiKey.substring(apiKey.indexOf('-') + 1, apiKey.length());
                 String url = "https://" + server + ".api.mailchimp.com/3.0/lists/" + listId + "/interest-categories";
-                if (extendedPropertyDefinition.getName().equals("category")) {
+                //Until release of Hotfixes
+                HttpResponse<JsonNode> response = Unirest.get(url)
+                        .basicAuth(null, apiKey)
+                        .queryString("count", "100")
+                        .asJson();
+                //Prepare object for easy use.
+                JSONObject jsonObject = response.getBody().getObject();
+                if (jsonObject != null) {
+                    JSONArray rawLists = jsonObject.getJSONArray("categories");
+                    for (int i = 0; i < rawLists.length(); i++) {
+                        JSONObject category = (JSONObject) rawLists.get(i);
+                        String mainCategoryName = category.getString("title");
+                        HttpResponse<JsonNode> subCategories = Unirest.get(url + "/" + category.get("id") + "/interests")
+                                .basicAuth(null, apiKey)
+                                .queryString("count", "100")
+                                .asJson();
+                        JSONObject jsonObject2 = subCategories.getBody().getObject();
+                        if (jsonObject2 != null) {
+                            JSONArray rawLists2 = jsonObject2.getJSONArray("interests");
+                            for (int j = 0; j < rawLists2.length(); j++) {
+                                JSONObject subCategory = (JSONObject) rawLists2.get(j);
+                                results.add(new ChoiceListValue(mainCategoryName + " - " + subCategory.getString("name") + " (" + subCategory.getInt("subscriber_count") + " subscriber(s))", subCategory.getString("id")));
+                            }
+                        }
+                    }
+                }
+
+                /*if (extendedPropertyDefinition.getName().equals("category")) {
                     HttpResponse<JsonNode> response = Unirest.get(url)
                             .basicAuth(null, apiKey)
                             .queryString("count", "100")
@@ -90,7 +117,7 @@ public class MailChimpInterestsInitializer implements ModuleChoiceListInitialize
                             results.add(new ChoiceListValue(subCategory.getString("name") + " (" + subCategory.getInt("subscriber_count") + " subscriber(s))", subCategory.getString("id")));
                         }
                     }
-                }
+                }*/
             }
         } catch (UnirestException e) {
             e.printStackTrace();
