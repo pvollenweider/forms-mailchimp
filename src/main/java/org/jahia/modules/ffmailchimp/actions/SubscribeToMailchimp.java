99b2d5f47ea6efe825e5ed0a1b625c2ab6833622
stefan <ssavu@jahia.com>
2017-07-14 14:13:30 -0400

    JAHIACOM-448 - Added check to make sure email field exists and is not empty.

--- src/main/java/org/jahia/modules/ffmailchimp/actions/SubscribeToMailchimp.java
+++ src/main/java/org/jahia/modules/ffmailchimp/actions/SubscribeToMailchimp.java
@@ -1,5 +1,6 @@
 package org.jahia.modules.ffmailchimp.actions;
 
+import com.drew.lang.StringUtil;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -97,70 +98,78 @@ public class SubscribeToMailchimp extends Action {
                     }
                 }
             }
-            //Add enabled Meta data merge fields
-            Map<SubmissionMetaData, String> mergeFieldExistsMap = SubmissionMetaData.getSubmissionMetaDataTypesAsMap();
-            for (Map.Entry<SubmissionMetaData, String> entry : mergeFieldExistsMap.entrySet()) {
-                SubmissionMetaData submissionMetaData = entry.getKey();
-                String mergeTag = submissionMetaData.toString();
-                if (mailchimpConfiguration.hasProperty(submissionMetaData.getJcrPropertyName()) && mailchimpConfiguration.getProperty(submissionMetaData.getJcrPropertyName()).getBoolean()) {
-                    switch (submissionMetaData) {
-                        case FFSERVER:
-                            mailchimpMergeFields.put(mergeTag, formNode.getResolveSite().getServerName());
-                            break;
-                        case FFREFERRER:
-                            String origin = req.getHeader("referer");
-                            mailchimpMergeFields.put(mergeTag, StringUtils.isNotEmpty(origin) ? origin : req.getRequestURI());
-                            break;
-                        case FFFORMID:
-                            mailchimpMergeFields.put(mergeTag, formNode.getPropertyAsString(Constants.JCR_TITLE));
-                            break;
+            if (!StringUtils.isEmpty(email)) {
+                //Add enabled Meta data merge fields
+                Map<SubmissionMetaData, String> mergeFieldExistsMap = SubmissionMetaData.getSubmissionMetaDataTypesAsMap();
+                for (Map.Entry<SubmissionMetaData, String> entry : mergeFieldExistsMap.entrySet()) {
+                    SubmissionMetaData submissionMetaData = entry.getKey();
+                    String mergeTag = submissionMetaData.toString();
+                    if (mailchimpConfiguration.hasProperty(submissionMetaData.getJcrPropertyName()) && mailchimpConfiguration.getProperty(submissionMetaData.getJcrPropertyName()).getBoolean()) {
+                        switch (submissionMetaData) {
+                            case FFSERVER:
+                                mailchimpMergeFields.put(mergeTag, formNode.getResolveSite().getServerName());
+                                break;
+                            case FFREFERRER:
+                                String origin = req.getHeader("referer");
+                                mailchimpMergeFields.put(mergeTag, StringUtils.isNotEmpty(origin) ? origin : req.getRequestURI());
+                                break;
+                            case FFFORMID:
+                                mailchimpMergeFields.put(mergeTag, formNode.getPropertyAsString(Constants.JCR_TITLE));
+                                break;
+                        }
                     }
                 }
-            }
-            String formDisplayId = parameters.get("formDisplayId").get(0);
+                String formDisplayId = parameters.get("formDisplayId").get(0);
 
-            //Update subscribe/update member in mailchimp
-            byte[] emailAsBytes = email.getBytes("UTF-8");
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            String emailMD5Hash = Hex.encodeHexString(md.digest(emailAsBytes));
-            String server = apiKey.substring(apiKey.indexOf('-') + 1, apiKey.length());
-            StringBuilder entryPointSb = new StringBuilder("https://");
-            entryPointSb.append(server).append(".api.mailchimp.com/3.0/lists/{listId}/members/{subscriberHash}");
-            JSONObject reqBody = new JSONObject();
-            reqBody.put("email_address", email)
-                    .put("status_if_new", "subscribed")
-                    .put("merge_fields", mailchimpMergeFields)
-                    .put("ip_signup", req.getRemoteAddr())
-                    .put("language", session.getLocale());
-            if (session.getNodeByIdentifier(formDisplayId).isNodeType("fcmix:mailchimpGroup")) {
-                JSONObject interests = new JSONObject();
-                JCRValueWrapper[] group = session.getNodeByIdentifier(formDisplayId).getProperty("group").getValues();
-                for (JCRValueWrapper valueWrapper : group) {
-                    interests.put(valueWrapper.getString(), Boolean.TRUE);
+                //Update subscribe/update member in mailchimp
+                byte[] emailAsBytes = email.getBytes("UTF-8");
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                String emailMD5Hash = Hex.encodeHexString(md.digest(emailAsBytes));
+                String server = apiKey.substring(apiKey.indexOf('-') + 1, apiKey.length());
+                StringBuilder entryPointSb = new StringBuilder("https://");
+                entryPointSb.append(server).append(".api.mailchimp.com/3.0/lists/{listId}/members/{subscriberHash}");
+                JSONObject reqBody = new JSONObject();
+                reqBody.put("email_address", email)
+                        .put("status_if_new", "subscribed")
+                        .put("merge_fields", mailchimpMergeFields)
+                        .put("ip_signup", req.getRemoteAddr())
+                        .put("language", session.getLocale());
+                if (session.getNodeByIdentifier(formDisplayId).isNodeType("fcmix:mailchimpGroup")) {
+                    JSONObject interests = new JSONObject();
+                    JCRValueWrapper[] group = session.getNodeByIdentifier(formDisplayId).getProperty("group").getValues();
+                    for (JCRValueWrapper valueWrapper : group) {
+                        interests.put(valueWrapper.getString(), Boolean.TRUE);
+                    }
+                    reqBody.put("interests", interests);
                 }
-                reqBody.put("interests", interests);
-            }
-            try {
-                HttpResponse<JsonNode> response = Unirest.put(entryPointSb.toString())
-                        .basicAuth(null, apiKey)
-                        .header("Content-Type", "application/json")
-                        .routeParam("listId", listId)
-                        .routeParam("subscriberHash", emailMD5Hash)
-                        .body(reqBody.toString())
-                        .asJson();
-                jsonAnswer.put("status", "success");
-                jsonAnswer.put("actionName", "subscribeToMailchimp");
-                JSONObject results = new JSONObject();
-                results.put("response", response.getBody().getObject());
-                results.put("submission", inputResults);
-                jsonAnswer.put("results", results);
-                jsonAnswer.put("message", "Subscribed/Updated user to Mailchimp successfully!");
-                jsonAnswer.put("code", HttpServletResponse.SC_OK);
-                actionResult.setJson(results);
-            } catch (UnirestException e) {
+                try {
+                    HttpResponse<JsonNode> response = Unirest.put(entryPointSb.toString())
+                            .basicAuth(null, apiKey)
+                            .header("Content-Type", "application/json")
+                            .routeParam("listId", listId)
+                            .routeParam("subscriberHash", emailMD5Hash)
+                            .body(reqBody.toString())
+                            .asJson();
+                    jsonAnswer.put("status", "success");
+                    jsonAnswer.put("actionName", "subscribeToMailchimp");
+                    JSONObject results = new JSONObject();
+                    results.put("response", response.getBody().getObject());
+                    results.put("submission", inputResults);
+                    jsonAnswer.put("results", results);
+                    jsonAnswer.put("message", "Subscribed/Updated user to Mailchimp successfully!");
+                    jsonAnswer.put("code", HttpServletResponse.SC_OK);
+                    actionResult.setJson(results);
+                } catch (UnirestException e) {
+                    jsonAnswer.put("status", "error");
+                    jsonAnswer.put("message", e.getMessage());
+                    jsonAnswer.put("code", HttpServletResponse.SC_BAD_REQUEST);
+                    actionResult.setResultCode(HttpServletResponse.SC_BAD_REQUEST);
+                }
+            } else {
                 jsonAnswer.put("status", "error");
-                jsonAnswer.put("message", e.getMessage());
+                jsonAnswer.put("message", "Mailchimp user email was not mapped correctly or is missing.");
                 jsonAnswer.put("code", HttpServletResponse.SC_BAD_REQUEST);
+                logger.error("Failed to execute Subscribe to Mailchimp due to: empty email field or mapping mismatch");
                 actionResult.setResultCode(HttpServletResponse.SC_BAD_REQUEST);
             }
         } else {
